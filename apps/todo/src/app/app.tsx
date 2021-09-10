35c7096df24efe05cd047e14455f0fe409736439
Sean Behan <codebam@riseup.net>
2021-09-09 21:39:38 -0400

    form to add todos

--- apps/todo/src/app/app.tsx
+++ apps/todo/src/app/app.tsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import styles from './app.module.scss';
+import axios from 'axios';
+
+const formInitialState = {
+  content: '',
+};
 
 export function App() {
+  const [form, setForm] = useState(formInitialState);
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios.post('http://127.0.0.1:3333/api', form);
+  };
+
+  const handleSetForm = ({ target: { name, value } }: any) => {
+    setForm((form) => ({ ...form, [name]: value }));
+  };
+
   return (
     <div className={styles.app}>
       <header className="flex">
         <h1>Welcome to todo!</h1>
       </header>
-      <main></main>
+      <main>
+        <form onSubmit={handleFormSubmit}>
+          <input onChange={handleSetForm} type="text" name="content" />
+          <input type="submit" value="Add Todo" />
+        </form>
+      </main>
     </div>
   );
 }
